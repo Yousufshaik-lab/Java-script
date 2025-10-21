e4d564083bca1075e9d2a3b84c286984f005347b
Yousufshaik-lab <shaikmohammadyousuf10@gmail.com>
2025-10-21 09:27:17 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+
+const accountID= 22222
+let accountEmail="yousuf@.google.com"
+var accountPassword= "1010"
+accountCity="nalgonda"
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+ 
+
+// accountID=44558 note: this not possible
+accountEmail="younus@45gmail.com"
+accountPassword= "0909"
+accountCity="hyderabad"
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+/*var is not used  because of  issues in block scope and functional scope*/
